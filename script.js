9d34bd6c020af8f20c76ffc68085eca617ae1618
nooriaAhsani216 <nooriaahsani216@gmail.com>
2026-04-29 20:38:20 +0430

    Add more data and loop on it

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
+// competitors-data
 const competitors = [
-    {
-    name:"Nooria",
-    booksTitle :["Deep work","Atomic Habit","Clean Code"],
-    totalPages:[250,150,400],
-    pagesRead:[200,50,10]
-},
-{
-    name:"Amina",
-    booksTitle :["Deep work","Atomic Habit","Clean Code"],
-    totalPages:[250,150,400],
-    pagesRead:[200,50,10]
-},
-{
-    name:"Salma",
-    booksTitle :["Deep work","Atomic Habit","Clean Code"],
-    totalPages:[250,150,400],
-    pagesRead:[200,50,10]
+  {
+    name: "Amina",
+    booksTitle: ["Deep work", "Atomic Habits", "Clean code"],
+    totalPages: [250, 300, 464],
+    pagesRead: [250, 100, 200],
+  },
+  {
+    name: "Salma",
+    booksTitle: ["The Alchemist", "Atomic Habits", "Rich Dad Poor Dad"],
+    totalPages: [208, 300, 464],
+    pagesRead: [180, 150, 100],
+  },
+  {
+    name: "Mina",
+    booksTitle: [
+      "Think and Grow Rich",
+      "The 7 Habits of Highly Effective People",
+      "Clean code",
+    ],
+    totalPages: [320, 381, 464],
+    pagesRead: [300, 280, 300],
+  },
+  {
+    name: "Tamana",
+    booksTitle: ["Start With Why", "The Psychology of Money", "Zero to One"],
+    totalPages: [256, 256, 224],
+    pagesRead: [220, 120, 160],
+  },
+];
+
+for(let i = 0 ; i < competitors.length ; i++){
+    let competitor = competitors[i];
+    const {name,booksTitle,totalPages,pagesRead} = competitor;
+    console.log("================================");
+    console.log("Competitor Name:",name);
+    console.log("Books:",booksTitle);
+    console.log("Total Pages:", totalPages);
+    console.log("Pages read:",pagesRead);
+    for(let j = 0; j< booksTitle.length ; j++){
+       let progress = calculateProgress(
+        pagesRead[j],totalPages[j]
+       );
+       console.log("Books:",booksTitle);
+    console.log(`Progress: ${progress.toFixed(2)} %`);
 }
-]
-for(let i= 0; i < competitors.length;i++ ){
-    const person = competitors[i];
-    console.log("Competitors:",person.name);
-    console.log("Books:", person.booksTitle);
-    console.log("Total Pages:",person.totalPages);
-    console.log("Read Pages:", person.pagesRead)
+    }
+function calculateProgress(pagesRead,totalPages){
+  let result = (pagesRead / totalPages ) * 100;
+  return result;
 }
+calculateProgress();
